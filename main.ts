1bc2c91416b5c7128b90d30961a11b99332b6a46
Abdul <abdulqadeerprofile@gmail.com>
2024-03-30 01:12:07 +0500

    number-guesser

---
+++ main.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log("Welcome-to-aq-coding");
+
+const randomnumber = Math.floor(Math.random() * 5 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        name:"userguessednumber",
+        message:"guess number ",
+        type:"number",
+        
+    },
+]);
+
+if(answer.userguessednumber === randomnumber){
+    console.log(chalk.blue(" Yes!! You Guessed It Right "));
+}
+
+else{
+    console.log("Try Again");
+}
